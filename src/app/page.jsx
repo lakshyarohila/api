8e36697c6b6ec9091c939c6c84370bcc69bcb15e
lakshyarohila <lakshyarohila99@gmail.com>
2025-02-05 21:06:59 +0530

    final

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,18 @@
 export default function Home() {
   return (
    <div className="w-full   h-screen items-center py-40 flex-col flex gap-3 bg-black">
-    <h1 className=" text-3xl  font-bold text-white">RANDOM APIS 😂</h1>
+    <h1 className=" text-3xl  font-bold text-white">RANDOM JOKES API 😂</h1>
     <p className="text-xl text-white font-semibold">Its A Fun Project For Testing Feel Free To Try . </p>
+    <div className="tenor-gif-embed" data-postid="13050170504203729642" data-share-method="host" data-aspect-ratio="0.566265" data-width="10%"><a href="https://tenor.com/view/cool-fun-white-cat-dance-cool-and-fun-times-gif-13050170504203729642">Cool Fun GIF</a>from <a href="https://tenor.com/search/cool-gifs">Cool GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+    <h1 className="text-2xl text-white font-mono">
+      <code>
+       Endpoint:- /api/jokes
+      </code>
+    </h1>
+    <div className="mt-36 text-white pl-96">
+
+    <p className="border border-gray-300 px-3 py-1">Working on Updates </p>
+    </div>
    </div>
   );
 }
